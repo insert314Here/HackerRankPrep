4771ba777da8dd8d4521ff19ad86d612a23354b0
insert314Here <89361627+insert314Here@users.noreply.github.com>
2021-12-09 12:02:10 -0500

    Add project files.

---
+++ Mini-MaxSum/Program.cs
@@ -0,0 +1,77 @@
+﻿/* Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. 
+ Then print the respective minimum and maximum values as a single line of two space-separated long integers. */
+
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
+    {
+        long sumMin = 0;
+        //minimum sum is 16 = 1+3+5+7
+        //sort list from least to greatest
+        arr.Sort();
+        
+        //adds the first 4 values
+        for (int i = 0; i < arr.Count - 1; i++)
+        {
+            sumMin += arr[i];
+        }
+
+        //maximum sum is 24 = 3+5+7+9
+        //sort list from greatest to least
+        long sumMax = 0;
+        arr.Reverse(); //sorts list from greatest to least
+        for(int j = 0; j < arr.Count - 1; j++)
+        {
+            sumMax += arr[j];
+        }
+
+        //print out the mini-max values
+        Console.WriteLine($"{sumMin} {sumMax}");
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        //List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+        List<int> arr = new List<int>();
+                        //arr.Add(3);
+                        //arr.Add(1);
+                        //arr.Add(7);
+                        //arr.Add(5);
+                        //arr.Add(9);
+
+        arr.Add(256741038);
+        arr.Add(623958417);
+        arr.Add(467905213);
+        arr.Add(714532089);
+        arr.Add(938071625);
+
+
+
+        Result.miniMaxSum(arr);
+    }
+}
---
+++ PlusMinusRatios/Program.cs
@@ -0,0 +1,58 @@
+﻿List<int> arr = new List<int>();
+arr.Add(1);
+arr.Add(1);
+arr.Add(0);
+arr.Add(-1);
+arr.Add(-1);
+
+
+plusMinus(arr);
+
+static void plusMinus(List<int> arr)
+{
+    //need to have the length of the list for ratio
+            //int listLength = 0;
+            //for (int i = 0; i < arr.Count() + 1; i++)
+            //{
+            //    listLength += i;
+            //}
+
+    //iterate through list to find negatives, positives, and zero
+    List<int> negative = new List<int>();
+    List<int> positive = new List<int>();
+    List<int> zero = new List<int>();
+    foreach (int numb in arr)
+    {
+        if (numb < 0)
+        {
+            negative.Add(numb);
+        }
+        else if (numb == 0)
+        {
+            zero.Add(numb);
+        }
+        else
+        {
+            positive.Add(numb);
+        }
+    }
+
+    //positive ratio
+    decimal posRatio = 0;
+    decimal posCount = positive.Count();
+    posRatio = posCount / arr.Count();
+    Console.WriteLine(posRatio.ToString("N6"));
+
+    //negative ratio
+    decimal negRatio = 0;
+    decimal negCount = negative.Count();
+    negRatio = negCount / arr.Count();
+    Console.WriteLine(negRatio.ToString("N6"));
+
+    //zero ratio
+    decimal zeroRatio = 0;
+    decimal zeroCount = zero.Count();
+    zeroRatio = zeroCount / arr.Count();
+    Console.WriteLine(zeroRatio.ToString("N6"));
+
+}
